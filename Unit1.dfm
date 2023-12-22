c22ed6c8e7c8f4c97598d10a7c3c6371a7de7ccb
cricri-pingouin <64480118+cricri-pingouin@users.noreply.github.com>
2023-12-22 12:25:27 +0000

    Add files via upload
    - Some minor changes/fixes (e.g. chronometer bug)
    - Added simple timer; needs a sound.wav in same folder as executable

--- Unit1.dfm
+++ Unit1.dfm
@@ -4,8 +4,8 @@ object Form1: TForm1
   BorderIcons = [biSystemMenu]
   BorderStyle = bsSingle
   Caption = 'Calendar'
-  ClientHeight = 271
-  ClientWidth = 267
+  ClientHeight = 299
+  ClientWidth = 269
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -16,33 +16,40 @@ object Form1: TForm1
   OnShow = FormShow
   PixelsPerInch = 96
   TextHeight = 13
-  object lbl1: TLabel
+  object lblDateDiff: TLabel
     Left = 8
     Top = 224
-    Width = 105
+    Width = 108
     Height = 13
-    Caption = 'Selected date is today'
+    Caption = 'Selected date is today.'
   end
-  object lbl2: TLabel
-    Left = 96
+  object lblChronoValue: TLabel
+    Left = 79
     Top = 248
-    Width = 57
+    Width = 74
     Height = 13
     Caption = '00:00:00'
   end
-  object lbl3: TLabel
+  object lblChrono: TLabel
     Left = 8
     Top = 248
-    Width = 82
-    Height = 13
+    Width = 65
+    Height = 18
     Caption = 'Chronometer:'
+  end
+  object lblTimer: TLabel
+    Left = 8
+    Top = 272
+    Width = 65
+    Height = 13
+    Caption = 'Timer:'
   end
   object cal1: TMonthCalendar
     Left = 8
     Top = 8
     Width = 251
     Height = 183
-    Date = 45280.554234131950000000
+    Date = 45280.427283611110000000
     Font.Charset = DEFAULT_CHARSET
     Font.Color = clWindowText
     Font.Height = -13
@@ -66,24 +73,58 @@ object Form1: TForm1
     TabOrder = 1
     OnChange = DateTimePicker1Change
   end
-  object btn1: TButton
-    Left = 208
-    Top = 240
-    Width = 51
+  object btnChronoStartStop: TButton
+    Left = 167
+    Top = 245
+    Width = 43
     Height = 21
     Caption = 'Start'
     TabOrder = 2
-    OnClick = btn1Click
+    OnClick = btnChronoStartStopClick
   end
-  object tmr1: TTimer
-    OnTimer = tmr1Timer
+  object btnTimerStartStop: TButton
+    Left = 167
+    Top = 272
+    Width = 43
+    Height = 21
+    Caption = 'Start'
+    TabOrder = 3
+    OnClick = btnTimerStartStopClick
+  end
+  object dtp1: TDateTimePicker
+    Left = 79
+    Top = 272
+    Width = 74
+    Height = 21
+    Date = 45282.000000000000000000
+    Time = 45282.000000000000000000
+    Kind = dtkTime
+    TabOrder = 4
+  end
+  object btnChronoReset: TButton
+    Left = 216
+    Top = 245
+    Width = 43
+    Height = 21
+    Caption = 'Reset'
+    TabOrder = 5
+    OnClick = btnChronoResetClick
+  end
+  object tmrCaption: TTimer
+    OnTimer = tmrCaptionTimer
     Left = 224
-    Top = 112
+    Top = 56
   end
-  object tmr2: TTimer
+  object tmrChrono: TTimer
     Enabled = False
-    OnTimer = tmr2Timer
+    OnTimer = tmrChronoTimer
     Left = 224
-    Top = 152
+    Top = 88
+  end
+  object tmrTimer: TTimer
+    Enabled = False
+    OnTimer = tmrTimerTimer
+    Left = 224
+    Top = 120
   end
 end
--- Unit1.pas
+++ Unit1.pas
@@ -3,31 +3,42 @@ unit Unit1;
 interface
 
 uses
-  SysUtils, Classes, Controls, Forms, ExtCtrls, ComCtrls, StdCtrls;
+  SysUtils, Classes, Controls, Forms, ExtCtrls, ComCtrls, StdCtrls, MMSystem;
 
 type
   TForm1 = class(TForm)
     cal1: TMonthCalendar;
     DateTimePicker1: TDateTimePicker;
-    lbl1: TLabel;
-    lbl2: TLabel;
-    lbl3: TLabel;
-    btn1: TButton;
-    tmr1: TTimer;
-    tmr2: TTimer;
+    tmrCaption: TTimer;
+    lblChrono: TLabel;
+    lblDateDiff: TLabel;
+    lblChronoValue: TLabel;
+    btnChronoStartStop: TButton;
+    btnChronoReset: TButton;
+    tmrChrono: TTimer;
+    lblTimer: TLabel;
+    dtp1: TDateTimePicker;
+    btnTimerStartStop: TButton;
+    tmrTimer: TTimer;
     procedure DateBetween;
     procedure FormShow(Sender: TObject);
     procedure cal1Click(Sender: TObject);
     procedure DateTimePicker1Change(Sender: TObject);
-    procedure btn1Click(Sender: TObject);
-    procedure tmr1Timer(Sender: TObject);
-    procedure tmr2Timer(Sender: TObject);
+    procedure btnChronoStartStopClick(Sender: TObject);
+    procedure btnChronoResetClick(Sender: TObject);
+    procedure btnTimerStartStopClick(Sender: TObject);
+    procedure tmrCaptionTimer(Sender: TObject);
+    procedure tmrChronoTimer(Sender: TObject);
+    procedure tmrTimerTimer(Sender: TObject);
+
   private
     { Private declarations }
   public
     { Public declarations }
-    Chrono: Boolean;
-    StartTime: TTime;
+    Chrono, Timer: Boolean;
+    ChronoTime: TTime;
+    const
+      TimerInterval = 1 / 24 / 60 / 60;
   end;
 
 var
@@ -54,7 +65,7 @@ begin
     Caption := Caption + FloatToStr(i) + ' days ago.'
   else
     Caption := Caption + FloatToStr(Abs(i)) + ' days from today.';
-  lbl1.Caption := Caption;
+  lblDateDiff.Caption := Caption;
 end;
 
 procedure TForm1.FormShow(Sender: TObject);
@@ -62,6 +73,7 @@ begin
   Self.Caption := TimeToStr(Time);
   cal1.Date := Date;
   DateTimePicker1.DateTime := Date;
+  ChronoTime := 0;
 end;
 
 procedure TForm1.cal1Click(Sender: TObject);
@@ -76,31 +88,65 @@ begin
   DateBetween;
 end;
 
-procedure TForm1.btn1Click(Sender: TObject);
+procedure TForm1.btnChronoStartStopClick(Sender: TObject);
 begin
   if not Chrono then
   begin
+    btnChronoStartStop.Caption := 'Stop';
     Chrono := True;
-    btn1.Caption := 'Stop';
-    StartTime := Time;
-    tmr2.enabled := True;
+    tmrChrono.enabled := True;
   end
   else
   begin
+    tmrChrono.enabled := False;
     Chrono := False;
-    btn1.Caption := 'Start';
-    tmr2.enabled := False;
+    btnChronoStartStop.Caption := 'Start';
   end;
 end;
 
-procedure TForm1.tmr1Timer(Sender: TObject);
+procedure TForm1.btnChronoResetClick(Sender: TObject);
+begin
+  ChronoTime := 0;
+  lblChronoValue.Caption := '00:00:00';
+end;
+
+procedure TForm1.btnTimerStartStopClick(Sender: TObject);
+begin
+  if not Timer then
+  begin
+    btnTimerStartStop.Caption := 'Stop';
+    dtp1.Date := 1000;
+    Timer := True;
+    tmrTimer.enabled := True;
+  end
+  else
+  begin
+    tmrTimer.enabled := False;
+    Timer := False;
+    btnTimerStartStop.Caption := 'Start';
+  end;
+end;
+
+procedure TForm1.tmrCaptionTimer(Sender: TObject);
 begin
   Form1.Caption := TimeToStr(Time);
 end;
 
-procedure TForm1.tmr2Timer(Sender: TObject);
+procedure TForm1.tmrChronoTimer(Sender: TObject);
 begin
-  lbl2.Caption := TimeToStr(Time - StartTime);
+  ChronoTime := ChronoTime + TimerInterval;
+  lblChronoValue.Caption := TimeToStr(ChronoTime);
+end;
+
+procedure TForm1.tmrTimerTimer(Sender: TObject);
+begin
+  dtp1.Time := dtp1.Time - TimerInterval;
+  if dtp1.Time >= 1000.9999 then
+  begin
+    dtp1.Time := 0;
+    sndPlaySound('sound.wav', SND_NODEFAULT or SND_ASYNC);
+    btnTimerStartStopClick(Self);
+  end;
 end;
 
 end.
